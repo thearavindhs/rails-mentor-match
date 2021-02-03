0152fa17b0789cc5870c511d8185818b875a8fc4
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-03 00:02:10 -0500

    created migration, added colums to the users table - added connections table and included has_many connections in user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_021228) do
+ActiveRecord::Schema.define(version: 2021_02_03_045733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "connections", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_connections_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +30,21 @@ ActiveRecord::Schema.define(version: 2021_02_03_021228) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "city"
+    t.string "country"
+    t.string "gender"
+    t.string "role"
+    t.string "industry"
+    t.string "language"
+    t.string "communication"
+    t.integer "experience"
+    t.string "user_type"
+    t.integer "maximum_mentee"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "connections", "users"
 end
