eb36819661a34cea822cacd4417cdb62d7506196
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-04 23:02:33 -0500

    added the title to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_045733) do
+ActiveRecord::Schema.define(version: 2021_02_05_040001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_045733) do
     t.integer "experience"
     t.string "user_type"
     t.integer "maximum_mentee"
+    t.string "title"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
