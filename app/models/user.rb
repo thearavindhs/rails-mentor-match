3e99692a5d5a6ec589f7e9f061a183d40b107575
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-07 13:19:39 -0500

    profile page backend html completion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :connections
+  has_many :connection_mentees, class_name: "Connection", foreign_key: "mentor_id"
+  has_many :mentees, through: :connection_mentees
+  has_one :connection_mentor, class_name: "Connection", foreign_key: "mentee_id"
+  has_one :mentor, through: :connection_mentor
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,12 @@ ActiveRecord::Schema.define(version: 2021_02_05_040651) do
   enable_extension "plpgsql"
 
   create_table "connections", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "mentor_id", null: false
+    t.bigint "mentee_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_connections_on_user_id"
+    t.index ["mentee_id"], name: "index_connections_on_mentee_id"
+    t.index ["mentor_id"], name: "index_connections_on_mentor_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +48,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_040651) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "connections", "users"
+  add_foreign_key "connections", "users", column: "mentee_id"
+  add_foreign_key "connections", "users", column: "mentor_id"
 end
