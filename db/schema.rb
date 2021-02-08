fdf730ee4baf14ca0d05ce87915933ccad4e07f3
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-07 20:28:53 -0500

    active storage attachments setup completion

--- db/schema.rb
+++ db/schema.rb
@@ -86,9 +86,9 @@ ActiveRecord::Schema.define(version: 2021_02_06_185546) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "connections", "users", column: "mentee_id"
   add_foreign_key "connections", "users", column: "mentor_id"
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "personal_skills", "skills"
   add_foreign_key "personal_skills", "users"
   add_foreign_key "skills", "personal_skills"
