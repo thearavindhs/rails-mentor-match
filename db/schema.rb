2710786b18f13105269d7f2030f1c9a2b0862db6
Kailene Chen <chen.kailene@gmail.com>
2021-02-20 13:01:28 -0500

    Finished all profiles pages minus the dynamic photos

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-# Provided tge details
 
 ActiveRecord::Schema.define(version: 2021_02_13_190511) do
 
@@ -38,10 +37,12 @@ ActiveRecord::Schema.define(version: 2021_02_13_190511) do
   end
 
   create_table "connections", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "mentor_id", null: false
+    t.bigint "mentee_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_connections_on_user_id"
+    t.index ["mentee_id"], name: "index_connections_on_mentee_id"
+    t.index ["mentor_id"], name: "index_connections_on_mentor_id"
   end
 
   create_table "personal_skills", force: :cascade do |t|
@@ -93,7 +94,8 @@ ActiveRecord::Schema.define(version: 2021_02_13_190511) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "connections", "users"
+  add_foreign_key "connections", "users", column: "mentee_id"
+  add_foreign_key "connections", "users", column: "mentor_id"
   add_foreign_key "personal_skills", "skills"
   add_foreign_key "personal_skills", "users"
   add_foreign_key "skills", "personal_skills"
