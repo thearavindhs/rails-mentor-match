e34b160b9307b9c325c6f8a6ee9b85abc94808b8
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-02-26 09:50:43 -0500

    linkedin setup complete to get first_name, last_name, profile_pic and email

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def linkedin
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Linkedin") if is_navigational_format?
+    else
+      session["devise.linkedin_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url
+    end
+  end
+  
+  def failure
+    redirect_to root_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[linkedin]
+
   def matchMe
     MatchAlgorithmJob.perform_now(self)
   end
@@ -19,4 +21,23 @@ class User < ApplicationRecord
   # validates :last_name, presence: true
   # validates :city, presence: true
   # validates :country, presence: true
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.linkedin_data"] && session["devise.linkedin_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.picture_url = auth.info.picture_url
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+  
 end
