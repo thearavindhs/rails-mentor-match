3bcb62224ef46a7ae886cdda47f7a45e75fa0a72
alberto romano <aromanog1@gmail.com>
2021-03-01 18:30:26 -0600

    Changing js for roles page

--- app/javascript/packs/role_preference.js
+++ app/javascript/packs/role_preference.js
@@ -1,3 +1,13 @@
 document.querySelector(".user_role_preference").hidden = true;
 const input = document.querySelector("#user_role_preference")
 input.value = "Tech";
+
+const cards = document.querySelectorAll(".role-card");
+
+cards.forEach((card) => {
+  card.style.cursor = "pointer"
+  card.addEventListener("click",(event) => {
+    console.log("I was clicked");
+  });
+});
+
