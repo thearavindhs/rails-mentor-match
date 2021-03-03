5ce0b6067c1db3c605b0827ce3c6e1bd6bc7e742
alberto romano <aromanog1@gmail.com>
2021-03-02 22:12:13 -0600

    Changed html and js

--- app/javascript/packs/role_preference.js
+++ app/javascript/packs/role_preference.js
@@ -1,13 +1,21 @@
 document.querySelector(".user_role_preference").hidden = true;
-const input = document.querySelector("#user_role_preference")
-input.value = "Tech";
+const inputForm = document.querySelector("#user_role_preference")
 
 const cards = document.querySelectorAll(".role-card");
 
 cards.forEach((card) => {
-  card.style.cursor = "pointer"
   card.addEventListener("click",(event) => {
-    console.log("I was clicked");
+    event.preventDefault();
+    inputForm.value = card.childNodes[1].innerText;
+    // deactivateCards();
+    console.log(`Input -> ${inputForm.value}`);
+    // card.childNodes[1].classList.replace("background-card-small", "background-card-small-active");
   });
 });
 
+// function deactivateCards(){
+//   // CurrentTarget is the complete card.
+//   cards.forEach((card) => {
+//     if (card.childNodes[1].classList.contains("")) {}
+//   });
+// }
