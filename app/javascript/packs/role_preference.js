81913f6185c5d64edf242e9e0e2a633ecf4050b6
alberto romano <aromanog1@gmail.com>
2021-03-04 20:17:54 -0600

    User can only select one card for roles

--- app/javascript/packs/role_preference.js
+++ app/javascript/packs/role_preference.js
@@ -4,6 +4,22 @@ const cards = document.querySelectorAll(".role-card");
 const rolePreference = document.querySelector("#user_role_preference");
 const role = document.querySelector("#user_role");
 
+function selectOneCard(selectedCard){
+  // Only select one card. Remove active from all the other cards.
+  cards.forEach((card) => {
+    if (card === selectedCard) {
+      card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
+      card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
+    } else {
+      if(card.childNodes[1].childNodes[1].classList.contains("role-name-large-active") || card.childNodes[1].childNodes[1].classList.contains("role-name-small-active")){
+        card.childNodes[1].childNodes[1].classList.replace("role-name-large-active", "role-name-large");
+        card.childNodes[1].childNodes[1].classList.replace("role-name-small-active", "role-name-small");
+      }
+    }
+  });
+}
+
+
 
 if (role === null) {
   // No role input.
@@ -14,13 +30,13 @@ if (role === null) {
   document.querySelector(".user_role_preference").hidden = true;
 
   cards.forEach((card) => {
+    card.style.cursor = 'pointer';
     card.addEventListener("click",(event) => {
       event.preventDefault();
       rolePreference.value = card.childNodes[1].innerText;
       // deactivateCards();
       console.log(`Input Role Preference -> ${rolePreference.value}`);
-      card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
-      card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
+      selectOneCard(card);
     });
   });
 
@@ -35,13 +51,12 @@ if (role === null) {
   document.querySelector(".user_role").hidden = true;
 
   cards.forEach((card) => {
+    card.style.cursor = 'pointer';
     card.addEventListener("click", (event) => {
       event.preventDefault();
       role.value = card.childNodes[1].innerText;
       console.log(`Input Role -> ${role.value}`);
-      card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
-      card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
-
+      selectOneCard(card);
     });
   });
 
