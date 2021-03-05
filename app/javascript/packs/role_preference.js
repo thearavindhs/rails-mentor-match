c44baadc98f1a9d9d5cfe98dcbb3a4ec9fab6f27
alberto romano <aromanog1@gmail.com>
2021-03-04 20:01:52 -0600

    Added roles input. I was missing that haha

--- app/javascript/packs/role_preference.js
+++ app/javascript/packs/role_preference.js
@@ -1,15 +1,48 @@
-document.querySelector(".user_role_preference").hidden = true;
-const inputForm = document.querySelector("#user_role_preference");
 
 const cards = document.querySelectorAll(".role-card");
 
-cards.forEach((card) => {
-  card.addEventListener("click",(event) => {
-    event.preventDefault();
-    inputForm.value = card.childNodes[1].innerText;
-    // deactivateCards();
-    console.log(`Input -> ${inputForm.value}`);
-    card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
-    card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
+const rolePreference = document.querySelector("#user_role_preference");
+const role = document.querySelector("#user_role");
+
+
+if (role === null) {
+  // No role input.
+  // Then, rolePreference is up.
+
+  //  ------------ Mentee Form --------------
+  rolePreference.hidden = true;
+  document.querySelector(".user_role_preference").hidden = true;
+
+  cards.forEach((card) => {
+    card.addEventListener("click",(event) => {
+      event.preventDefault();
+      rolePreference.value = card.childNodes[1].innerText;
+      // deactivateCards();
+      console.log(`Input Role Preference -> ${rolePreference.value}`);
+      card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
+      card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
+    });
   });
-});
+
+
+
+} else if(rolePreference === null) {
+  // No role Preference input.
+  // Then, role is up.
+
+  //  ------------ Mentor Form --------------
+  role.hidden = true;
+  document.querySelector(".user_role").hidden = true;
+
+  cards.forEach((card) => {
+    card.addEventListener("click", (event) => {
+      event.preventDefault();
+      role.value = card.childNodes[1].innerText;
+      console.log(`Input Role -> ${role.value}`);
+      card.childNodes[1].childNodes[1].classList.replace("role-name-large", "role-name-large-active");
+      card.childNodes[1].childNodes[1].classList.replace("role-name-small", "role-name-small-active");
+
+    });
+  });
+
+}
