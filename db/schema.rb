c5fbd8f0bf10a6503ab2472fe1b3c2e3707adcc3
thearavindhs <j.aravindhrajkumar@gmail.com>
2021-03-04 22:57:54 -0500

    master2 changes to DB

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,12 @@ ActiveRecord::Schema.define(version: 2021_02_26_144122) do
   end
 
   create_table "connections", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "mentor_id", null: false
+    t.bigint "mentee_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_connections_on_user_id"
+    t.index ["mentee_id"], name: "index_connections_on_mentee_id"
+    t.index ["mentor_id"], name: "index_connections_on_mentor_id"
   end
 
   create_table "personal_skills", force: :cascade do |t|
@@ -98,7 +100,8 @@ ActiveRecord::Schema.define(version: 2021_02_26_144122) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "connections", "users"
+  add_foreign_key "connections", "users", column: "mentee_id"
+  add_foreign_key "connections", "users", column: "mentor_id"
   add_foreign_key "personal_skills", "skills"
   add_foreign_key "personal_skills", "users"
   add_foreign_key "skills", "personal_skills"
